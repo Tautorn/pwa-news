e7575865f29cc5f4270ab1696bb4d23d61987cc1
Tautorn <brunocarneirofs@gmail.com>
2020-07-18 16:00:28 -0300

    sw inside app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import {
   Route
 } from "react-router-dom"
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./worker.js').then(function(registration) {
+      console.log('Worker registration successful', registration.scope);
+    }, function(err) {
+      console.log('Worker registration failed', err);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  });
+} else {
+  console.log('Service Worker is not supported by browser.');
+}
+
 function App() {
   return (
     <main>
