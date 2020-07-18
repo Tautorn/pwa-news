15d9236dd67eec0b37d844fa52132e70fc3ac57a
Tautorn <brunocarneirofs@gmail.com>
2020-07-18 18:53:17 -0300

    v1 cache

--- public/worker.js
+++ public/worker.js
@@ -16,47 +16,29 @@ self.addEventListener('install', event => {
   );
 });
 
-// Update a service worker
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['/'];
+this.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['v2'];
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
           return response;
-        }
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+        });  
+      });
+    })
+  );
 });
