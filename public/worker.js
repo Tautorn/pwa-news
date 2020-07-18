0badad8c4844810df54dd89b8607c805db07ff1b
Tautorn <brunocarneirofs@gmail.com>
2020-07-18 16:29:54 -0300

    update sw

--- public/worker.js
+++ public/worker.js
@@ -17,18 +17,48 @@ self.addEventListener('install', event => {
 });
 
 // Update a service worker
-self.addEventListener("activate", function(event) {
-  console.log("[Servicework] Activate");
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['/'];
+
   event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log("[ServiceWorker] Removing old cache shell", key);
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
